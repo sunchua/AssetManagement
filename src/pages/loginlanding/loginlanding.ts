53c344f5be73b36a00a8a8b9983a54bd1b8a182a
sunchua <vivek.sunchu@cyient.com>
2018-11-08 00:51:17 +0530

    Sample ionic test app
    Initial test push

---
+++ src/pages/loginlanding/loginlanding.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-loginlanding',
+  templateUrl: 'loginlanding.html'
+})
+export class loginlandingPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
